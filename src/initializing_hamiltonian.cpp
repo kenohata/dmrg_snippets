836a9f375a9c22d821d04b77c143dab4014fcfaf
Ken Ohata <ohata@daraf.co.jp>
2013-11-22 09:44:40 +0900

    update initializing_hamiltonian
    define nthSpinZOperator

--- src/initializing_hamiltonian.cpp
+++ src/initializing_hamiltonian.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <Eigen/Core>
 #include <Eigen/Eigenvalues>
-#define BODY_N   4
-#define STATE_N 16
-
-typedef Eigen::Matrix<float, STATE_N, STATE_N> Matrix;
+#define BODY_N  2
+#define STATE_N 4
 
 bool isNthSpinUp(int state, int n){
   return (state >> n & 1) == 1;
@@ -14,37 +12,37 @@ bool isNthSpinDown(int state, int n){
   return (state >> n & 1) == 0;
 }
 
+float nthSpinZOperator(int state, int n){
+  if(isNthSpinUp(state, n)){
+    return 0.5;
+  }else{
+    return - 0.5;
+  }
+}
+
 int replaceUpDownToDownUp(int state, int n){
-  return state - (0b1 << (n + 1)) + (0b1 << n);
+  return state - (1 << (n + 1)) + (1 << n);
 }
 
 int replaceDownUpToUpDown(int state, int n){
-  return state + (0b1 << (n + 1)) - (0b1 << n);
+  return state + (1 << (n + 1)) - (1 << n);
 }
 
 int main(){
-  Matrix hamiltonian = Matrix::Zero();
+  Eigen::MatrixXd hamiltonian = Eigen::MatrixXd::Zero(STATE_N, STATE_N);
 
-  // Each matrix rows
   for (int i = 0; i < STATE_N; ++i){
-    // Each body
     for (int j = 0; j < BODY_N - 1; ++j){
-      if (isNthSpinDown(i, j + 1) && isNthSpinDown(i, j)){
-        // down down
-        hamiltonian(i, i) += 0.25;
-      }else if(isNthSpinDown(i, j + 1) && isNthSpinUp(i, j)){
+      hamiltonian(i, i) += nthSpinZOperator(i, j) * nthSpinZOperator(i, j + 1);
+
+      if(isNthSpinDown(i, j + 1) && isNthSpinUp(i, j)){
         // down up
         int l = replaceDownUpToUpDown(i, j);
         hamiltonian(i, l) += 0.5;
-        hamiltonian(i, i) -= 0.25;
       }else if(isNthSpinUp(i, j + 1) && isNthSpinDown(i, j)){
         // up down
         int l = replaceUpDownToDownUp(i, j);
         hamiltonian(i, l) += 0.5;
-        hamiltonian(i, i) -= 0.25;
-      }else if(isNthSpinUp(i, j + 1) && isNthSpinUp(i, j)){
-        // up up
-        hamiltonian(i, i) += 0.25;
       }
     }
   }
